644c1f58e36dc7d2092fe4a2c42ee12e62ecaf07
算神 <lishewen@msn.com>
2024-05-07 11:59:00 +0800

    尝试通过`CustomTransactionProcedure`来修补因`Garnet`缺失`ZREVRANGEBYLEX`和`ZRANGEBYLEX`指令而导致`Microsoft.Orleans.Reminders.Redis`提示`ERR unknown command`错误，在`rediscli`中测试命令正常，但通过`Microsoft.Orleans.Reminders.Redis`调用请求时报`ERR Transaction failed.`

---
+++ JT808.GPSTracker.GarnetServer/Extensions/ZRANGEBYLEXCommand.cs
@@ -0,0 +1,28 @@
+﻿using Garnet.common;
+using Garnet.server;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JT808.GPSTracker.GarnetServer.Extensions
+{
+    public class ZRANGEBYLEXCommand : CustomTransactionProcedure
+    {
+        public override void Main<TGarnetApi>(TGarnetApi api, ArgSlice input, ref MemoryResult<byte> output) => throw new InvalidOperationException();
+
+        public override bool Prepare<TGarnetReadApi>(TGarnetReadApi api, ArgSlice input) => false;
+
+        public override void Finalize<TGarnetApi>(TGarnetApi api, ArgSlice input, ref MemoryResult<byte> output)
+        {
+            int offset = 0;
+            var key = GetNextArg(input, ref offset);
+            var min = GetNextArg(input, ref offset);
+            var max = GetNextArg(input, ref offset);
+            api.SortedSetRange(key, min, max, SortedSetOrderOperation.ByLex, out var outputvalues, out _);
+
+            WriteBulkStringArray(ref output, outputvalues);
+        }
+    }
+}
--- JT808.GPSTracker.GarnetServer/Program.cs
+++ JT808.GPSTracker.GarnetServer/Program.cs
@@ -1,14 +1,22 @@
 ﻿using Garnet;
+using JT808.GPSTracker.GarnetServer.Extensions;
 
 Console.Title = "JT808.GPSTracker.GarnetServer";
 try
 {
     if (args.Length == 0) args = ["--config-import-path", "./garnet.conf"];
     using var server = new GarnetServer(args);
+    RegisterExtensions(server);
     server.Start();
     Thread.Sleep(Timeout.Infinite);
 }
 catch (Exception ex)
 {
     Console.WriteLine($"Unable to initialize server due to exception: {ex.Message}");
+}
+
+static void RegisterExtensions(GarnetServer server)
+{
+    server.Register.NewTransactionProc("ZRANGEBYLEX", 3, () => new ZRANGEBYLEXCommand());
+    server.Register.NewTransactionProc("ZREVRANGEBYLEX", 3, () => new ZREVRANGEBYLEXCommand());
 }
